681d21616bce4efffd634bad802becc636463361
zSouul <laxor003@gmail.com>
2026-08-06 22:31:15 +0200

    Add client entrypoint

---
+++ src/main/java/com/zsouul/shiftclicktotem/ShiftClickTotem.java
@@ -0,0 +1,10 @@
+package com.zsouul.shiftclicktotem;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShiftClickTotem implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// All the logic lives in HandledScreenMixin - nothing to set up here.
+	}
+}
